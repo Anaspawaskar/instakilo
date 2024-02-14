57864d1f7c310bff2df0a4f94079fb3f7fa48131
Anas Pawaskar <pawaskaranas10@gmail.com>
2024-02-14 22:13:37 +0530

    added

--- instakilo/instakilo/settings.py
+++ instakilo/instakilo/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = "instakilo.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
+        "ENGINE": os.environ.get("DB_ENGINE"),
         "NAME": os.environ.get("DB_NAME"),
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASS"),
